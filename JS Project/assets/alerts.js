763bcbdba34327a41761def805308738268bcbd5
Leon <leonmavrin181@gmail.com>
2025-03-29 21:56:37 +0300

    lesson1 tasks

---
+++ JS Project/assets/alerts.js
@@ -0,0 +1,17 @@
+
+//Alerts:
+
+//Task #1:
+
+/*
+let userName = prompt(`What's your names?`);
+alert(`Your name is `+userName+`!`);
+*/
+
+//Tsak #2:
+
+let deleteItem = confirm("Are you sure want to delete this item?");
+
+if (deleteItem) {
+  alert('Your item is deleted!');
+}
